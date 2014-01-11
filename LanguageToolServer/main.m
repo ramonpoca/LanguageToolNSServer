d552b00f436389388edf54d8af7c0979a28c24ac
Ramon Poca <ramon.poca@gmail.com>
2014-01-11 12:53:14 +0100

    Initial Commit

---
+++ LanguageToolServer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  LanguageToolServer
+//
+//  Created by Ramon Poca on 11/01/14.
+//  Copyright (c) 2014 Ramon Poca. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
