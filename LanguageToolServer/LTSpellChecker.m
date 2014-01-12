466ca29ba0d25e3d00ceb2fd63a46dd40fb472b5
Ramon Poca <ramon.poca@gmail.com>
2014-01-12 12:15:44 +0100

    Clean up garbage comments

--- LanguageToolServer/LTSpellChecker.m
+++ LanguageToolServer/LTSpellChecker.m
@@ -9,61 +9,20 @@
 
 
 @implementation LTSpellChecker {
-
 }
-/*
+/**
+  Convert errors from LanguageTool in this format
+    <error fromy="0" fromx="0" toy="0" tox="2" ruleId="UPPERCASE_SENTENCE_START"
+    msg="This sentence does not start with an uppercase letter"
+    replacements="My" context="my text"
+    contextoffset="0" offset="0" errorlength="2"
+    category="Capitalization" locqualityissuetype="typographical"/>
 
+  To the output dictionary expected by NSSpellChecker.
 
-- (NSArray *)spellServer:(NSSpellServer *)sender suggestGuessesForWord:(NSString *)word inLanguage:(NSString *)language {
-    return nil;
-}
-
-- (void)spellServer:(NSSpellServer *)sender didLearnWord:(NSString *)word inLanguage:(NSString *)language {
-
-}
-
-- (void)spellServer:(NSSpellServer *)sender didForgetWord:(NSString *)word inLanguage:(NSString *)language {
-
-}
-
-- (NSArray *)spellServer:(NSSpellServer *)sender suggestCompletionsForPartialWordRange:(NSRange)range inString:(NSString *)string language:(NSString *)language {
-    return nil;
-}
-
-- (void)spellServer:(NSSpellServer *)sender recordResponse:(NSUInteger)response toCorrection:(NSString *)correction forWord:(NSString *)word language:(NSString *)language {
-
-}
-
-- (NSArray *)spellServer:(NSSpellServer *)sender checkString:(NSString *)stringToCheck offset:(NSUInteger)offset types:(NSTextCheckingTypes)checkingTypes options:(NSDictionary *)options orthography:(NSOrthography *)orthography wordCount:(NSInteger *)wordCount {
-
-
-
-    return nil;
-}
+  Important: We return the whole input string as a matched range, if not done this way
+  NSSpellChecker keeps sending pieces of the string after the first match. ¬¬
 */
-/*
-NSGrammarRange
-The value for the NSGrammarRange dictionary key should be an NSValue containing an NSRange,
-a subrange of the sentence range used as the return value, whose location should be an offset
-from the beginning of the sentence--so, for example, an NSGrammarRange for the first four
-characters of the overall sentence range should be {0, 4}. If the NSGrammarRange key is not present in the dictionary it is assumed to be equal to the overall sentence range.
-NSGrammarUserDescription
-The value for the NSGrammarUserDescription dictionary key should be an NSString containing
-descriptive text about that range, to be presented directly to the user; it is intended that
-the user description should provide enough information to allow the user to correct the problem.
-It is recommended that NSGrammarUserDescription be supplied in all cases, however,
-NSGrammarUserDescription or NSGrammarCorrections must be supplied in order for correction
- guidance to be presented to the user.
-NSGrammarCorrections
-The value for the NSGrammarCorrections key should be an NSArray of NSStrings representing potential substitutions to correct the problem, but it is expected that this may not be available in all cases. NSGrammarUserDescription or NSGrammarCorrections must be supplied in order for correction guidance to be presented to the user.
-
-<error fromy="0" fromx="0" toy="0" tox="2" ruleId="UPPERCASE_SENTENCE_START"
-msg="This sentence does not start with an uppercase letter"
-replacements="My" context="my text"
-contextoffset="0" offset="0" errorlength="2"
-category="Capitalization" locqualityissuetype="typographical"/>
-
- */
 
 - (NSRange)spellServer:(NSSpellServer *)sender checkGrammarInString:(NSString *)stringToCheck language:(NSString *)language details:(NSArray **)details {
     NSRange result = NSMakeRange(NSNotFound, 0);
@@ -90,26 +49,9 @@ category="Capitalization" locqualityissuetype="typographical"/>
         }
         *details = errorDetails;
     }
+    //    return result; Wrong...
     return NSMakeRange(0, stringToCheck.length);
 
-//    return result;
-}
-
-
-
-/*
-- (NSRange)spellServer:(NSSpellServer *)sender findMisspelledWordInString:(NSString *)stringToCheck language:(NSString *)language wordCount:(NSInteger *)wordCount countOnly:(BOOL)countOnly {
-    NSRange result = NSMakeRange(NSNotFound, 0);
-    NSLog(@"Spelling req: %@", stringToCheck);
-    return result;
-} */
-
-- (NSString *)codeForLanguage:(NSString *)language {
-    return [@{
-             @"Català":@"ca",
-             @"Español":@"es",
-             @"English":@"en"
-             } objectForKey:language];
 }
 
 @end
